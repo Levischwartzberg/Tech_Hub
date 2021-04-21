bc46aea916c0695524847f201d7d31de55150bc4
Levi Schwartzberg <Levischwartzberg@gmail.com>
2021-04-21 01:31:40 -0500

    began working on editing existing posts

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -15,6 +15,22 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const updatedPost = await Post.update({
+      ...req.body,
+      user_id: req.session.user_id},
+        {where: {
+          id: req.params.id,
+        },
+    });
+
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const postData = await Post.destroy({
--- public/js/profile.js
+++ public/js/profile.js
@@ -5,7 +5,7 @@ const newFormHandler = async (event) => {
   const content = document.querySelector('#post-content').value.trim();
 
   if (title && content) {
-    const response = await fetch(`/api/posts`, {
+    const response = await fetch(`/api/posts/`, {
       method: 'POST',
       body: JSON.stringify({ title, content}),
       headers: {
@@ -21,8 +21,32 @@ const newFormHandler = async (event) => {
   }
 };
 
+const updateHandler = async (event) => {
+  event.preventDefault();
+  console.log("test");
+  const title = document.querySelector('#post-title').value.trim();
+  const content = document.querySelector('#post-content').value.trim();
+  const id = document.getElementById("update-btn").getAttribute('data-id');
+
+  if (title && content) {
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({ title, content}),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to edit post');
+    }
+  }
+};
+
 const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
+  if (event.target.getAttribute('id') === "delete") {
     const id = event.target.getAttribute('data-id');
 
     const response = await fetch(`/api/posts/${id}`, {
@@ -37,10 +61,33 @@ const delButtonHandler = async (event) => {
   }
 };
 
+const updatePostHandler = async (event) => {
+  event.preventDefault();
+  if (event.target.getAttribute('id') === "update") {
+    const id = event.target.getAttribute('data-id');
+
+    document.getElementById("update/create").textContent = "Update Post";
+    document.getElementById("update-btn").setAttribute("style", "display: block;")
+    document.getElementById("update-btn").setAttribute("data-id", id)
+    document.getElementById("create-btn").setAttribute("style", "display: none;")
+
+    document.querySelector('#post-title').defaultValue = document.getElementById('posttitle').textContent;
+    document.querySelector('#post-content').textContent = document.getElementById('postcontent').textContent;
+  }
+};
+
 document
   .querySelector('.new-post-form')
   .addEventListener('submit', newFormHandler);
 
 document
+  .querySelector('.new-post-form')
+  .addEventListener('submit', updateHandler);
+
+document
   .querySelector('.post-list')
   .addEventListener('click', delButtonHandler);
+
+  document
+  .querySelector('.post-list')
+  .addEventListener('click', updatePostHandler);
